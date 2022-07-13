2dd5c76897c5b2171a3388b0e4b9effd82e73d80
Anand V V <anand.vijay@gmail.com>
2022-07-13 17:33:25 -0400

    Added movie details page

--- movie-client/src/App.tsx
+++ movie-client/src/App.tsx
@@ -3,21 +3,19 @@ import './App.css';
 import NavigationBar from './components/NavigationBar';
 import MovieList from './components/MovieList';
 import Favorites from './components/Favorites';
-import { 
-  RouteComponentProps,
+import {
   BrowserRouter as Router,
+  Routes,
   Route,
-  Switch,
-  NavLink } from 'react-router-dom';
+  useRoutes,
+} from "react-router-dom";
+import MovieDetails from './components/MovieDetails';
 
-type Props = {};
-
-function App(props: Props) {
+const InnerApp = () => {
   const [movies, setMovies] = useState([]);
   const [currentApi, setCurrentApi] = useState("");
-  const [baseUrl] = useState("http://localhost:3002");
+  const [baseAPIUrl] = useState("http://localhost:3002");
   const [showFavoriteButtons, setShowFavoriteButtons] = useState(true);
-  const [countID, setCountID] = useState(0);
 
   console.log('current URL 👉️', window.location.href);
   console.log('current Pathname 👉️', window.location.pathname);
@@ -26,40 +24,46 @@ function App(props: Props) {
     let apiEndPoint = "";
     
     switch(window.location.pathname){
-      case "/coming-soon":
-        apiEndPoint = `${baseUrl}/movies-coming`;
+      case "/movies-coming":
+        // apiEndPoint = `${baseAPIUrl}/movies-coming`;
+        apiEndPoint = `/movies-coming`;
+        // apiEndPoint = `${window.location.href}`;
         setShowFavoriteButtons(true);
         setCurrentApi(apiEndPoint);
         break;
       case "/in-theaters":
-        apiEndPoint = `${baseUrl}/movies-in-theaters`;
+        // apiEndPoint = `${baseAPIUrl}/movies-in-theaters`;
+        apiEndPoint = `/movies-in-theaters`;
         setShowFavoriteButtons(true);
         setCurrentApi(apiEndPoint);
         break;
       case "/top-rated-india":
-        apiEndPoint = `${baseUrl}/top-rated-india`;
+        // apiEndPoint = `${baseAPIUrl}/top-rated-india`;
+        apiEndPoint = `/top-rated-india`;
         setShowFavoriteButtons(true);
         setCurrentApi(apiEndPoint);
         break;
       case "/top-rated-movies":
-        apiEndPoint = `${baseUrl}/top-rated-movies`;
+        // apiEndPoint = `${baseAPIUrl}/top-rated-movies`;
+        apiEndPoint = `/top-rated-movies`;
         setShowFavoriteButtons(true);
         setCurrentApi(apiEndPoint);
         break;
       case "/favorites":
-        apiEndPoint = `${baseUrl}/favourit`;
+        // apiEndPoint = `${baseAPIUrl}/favourit`;
+        apiEndPoint = `/favourit`;
         setShowFavoriteButtons(false);
         setCurrentApi(apiEndPoint);
         break;
       default: 
-        apiEndPoint = `${baseUrl}/movies-coming`;
+        // apiEndPoint = `${baseAPIUrl}/movies-coming`;
+        apiEndPoint = `/movies-coming`;
         setShowFavoriteButtons(true);
+        setCurrentApi(apiEndPoint);
         break;
     }
 
-    const url = apiEndPoint;
-
-		const response = await fetch(url);
+		const response = await fetch(`${baseAPIUrl}${apiEndPoint}`);
 		const responseJson = await response.json();
     console.log(responseJson);
     
@@ -73,13 +77,45 @@ function App(props: Props) {
 		getMovieRequest();
 	}, []);
 
+  return useRoutes([
+
+    {path: "/:category/:id", element: <MovieDetails /> },
+    {path:"/movies-coming", element: <MovieList movies={movies} currentApi={currentApi} baseUrl={baseAPIUrl} showFavoriteButtons={showFavoriteButtons} />},
+    {path:"/in-theaters", element: <MovieList movies={movies} currentApi={currentApi} baseUrl={baseAPIUrl} showFavoriteButtons={showFavoriteButtons} />},
+    {path:"/top-rated-india", element: <MovieList movies={movies} currentApi={currentApi} baseUrl={baseAPIUrl} showFavoriteButtons={showFavoriteButtons} />},
+    {path:"/top-rated-movies", element: <MovieList movies={movies} currentApi={currentApi} baseUrl={baseAPIUrl} showFavoriteButtons={showFavoriteButtons} />},
+    {path:"/favorites", element: <MovieList movies={movies} currentApi={currentApi} baseUrl={baseAPIUrl} showFavoriteButtons={showFavoriteButtons} />},
+    {path:"/", element: <MovieList movies={movies} currentApi={currentApi} baseUrl={baseAPIUrl} showFavoriteButtons={showFavoriteButtons} />}
+
+  ]);
+  
+  // return (
+  //   <>
+  //       <NavigationBar />
+  //       {/* <Routes>
+  //         <Route path="/:category/:id" element={<MovieDetails />} />
+  //         <Route path="/in-theaters" element={<MovieList movies={movies} currentApi={currentApi} baseUrl={baseUrl} showFavoriteButtons={showFavoriteButtons} />} />
+  //         <Route path="/coming-soon" element={<MovieList movies={movies} currentApi={currentApi} baseUrl={baseUrl} showFavoriteButtons={showFavoriteButtons} />} />
+  //         <Route path="/top-rated-india" element={<MovieList movies={movies} currentApi={currentApi} baseUrl={baseUrl} showFavoriteButtons={showFavoriteButtons} />} />
+  //         <Route path="/top-rated-movies" element={<MovieList movies={movies} currentApi={currentApi} baseUrl={baseUrl} showFavoriteButtons={showFavoriteButtons} />} />
+  //         <Route path="/favourit" element={<MovieList movies={movies} currentApi={currentApi} baseUrl={baseUrl} showFavoriteButtons={showFavoriteButtons} />} />
+  //         <Route path="/" element={<MovieList movies={movies} currentApi={currentApi} baseUrl={baseUrl} showFavoriteButtons={showFavoriteButtons} />} />
+  //       </Routes> */}
+  //       {/* <MovieList movies={movies} currentApi={currentApi} baseUrl={baseUrl} showFavoriteButtons={showFavoriteButtons} countID={countID}/> */}
+    
+  //   </>
+  // );
+}
+
+const App = () => {
   return (
     <>
-        <NavigationBar />
-        <MovieList movies={movies} currentApi={currentApi} baseUrl={baseUrl} showFavoriteButtons={showFavoriteButtons} countID={countID}/>
-    
+      <NavigationBar />
+      <Router>
+        <InnerApp />
+      </Router>
     </>
   );
-}
+};
 
 export default App;
